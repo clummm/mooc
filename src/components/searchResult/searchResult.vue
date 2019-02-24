2c0583acd7dce90d9c355c6eb24f50101f2194bc
zhangbo <1668417047@qq.com>
2019-02-24 19:38:17 +0800

    搜索结果页

---
+++ src/components/searchResult/searchResult.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>搜索{{this.searchContent}}</div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'searchResult',
+    data () {
+      return {
+        searchContent: ''
+      }
+    },
+    created () {
+      this.searchContent = this.$route.query.searchContent
+      console.log(this.$route.query)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
