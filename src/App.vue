70e6686b531e3503b7566cd60a972a383fece69b
zhangbo <1668417047@qq.com>
2019-02-24 19:38:52 +0800

    引入stylus

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,27 @@
   <div id="app">
     <v-header></v-header>
     <router-view/>
-    <v-footer></v-footer>
+    <account></account>
+    <v-footer v-if=this.showAccountWindow></v-footer>
   </div>
 </template>
 
 <script>
   import header from './components/header/header'
   import footer from './components/footer/footer'
+  import account from './components/account/account'
 
   export default {
     name: 'App',
+    data () {
+      return {
+        showAccountWindow: false
+      }
+    },
     components: {
       'v-header': header,
-      'v-footer': footer
+      'v-footer': footer,
+      account
     },
     created () {
       // 测试跨域请求
