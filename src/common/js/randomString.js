4f2c3bb106c3fa266e733107e617eea09ab6cd29
zhangbo <1668417047@qq.com>
2019-02-25 19:54:48 +0800

    2.25 基本完成登录注册部分

---
+++ src/common/js/randomString.js
@@ -0,0 +1,10 @@
+// 生成验证码的随机4位字符
+export function randomString (len) {
+  var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678'
+  var maxPos = $chars.length
+  var pwd = ''
+  for (let i = 0; i < len; i++) {
+    pwd += $chars.charAt(Math.floor(Math.random() * maxPos))
+  }
+  return pwd
+}
---
+++ src/components/help/protocol/protocol.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>慕课网用户注册协议</div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'protocol'
+  }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
