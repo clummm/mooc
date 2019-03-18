8b846108597267f69b247e8131ac619bc37d6ccb
zhangbo <1668417047@qq.com>
2019-03-18 22:49:56 +0800

    完成个人设置部分

--- src/common/js/data.js
+++ src/common/js/data.js
@@ -6,12 +6,12 @@ const USER_INFO = {
   phone: '13676010795',
   name: '张三',
   icon: AVATAR,
-  sex: '男',
+  sex: null,
   age: ['1995', '12', '22'],
   job: 'android开发工程师',
-  address: ['四川', '成都', '天府新区'],
+  address: ['四川', '成都'],
   interest: [1, 2, 3],
-  cuorses: [{
+  courses: [{
     id: '100',
     icon: COURSE,
     name: 'Unity5.x 创造 3D VR游戏'
